2496885c3ca03fa03cba8fedf80503eb66f6c0c2
David R. Saeva <drsaeva@gmail.com>
2016-09-30 18:17:03 -0400

    Create MorseCode.java

---
+++ WEP-ITPJ/MorseCode.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * MorseCode
+ * A class to translate messages to and from morse code
+ * @author David Saeva
+ *
+ */
+public class MorseCode {
+	
+	public static final String[] alphabet = {
+			"A", "B", "C", "D", "E", "F", "G", "H", 
+			"I", "J", "K", "L", "M", "N", "O", "P",
+			"Q", "R", "S", "T", "U", "V", "W", "X",
+			"Y", "Z", "1", "2", "3", "4", "5", "6",
+			"7", "8", "9", "10"
+	};
+	
+	public static final String[] code = {
+		".-", "-..", "-.-.", "-..", ".", "..-.", "--.", "....",
+		"..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+		"--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-",
+		"-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....",
+		"--...", "---..", "----.", "-----"
+ 	};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+
+	}
+
+	
+	
+}
