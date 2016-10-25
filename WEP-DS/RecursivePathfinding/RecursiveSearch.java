3e4431be47d6dfba87ebbaed06f09c55fdb91e59
David R. Saeva <drsaeva@gmail.com>
2016-10-25 17:35:12 -0400

    Create RecursiveSearch.java

---
+++ WEP-DS/RecursivePathfinding/RecursiveSearch.java
@@ -0,0 +1,19 @@
+package lab2;
+
+import java.util.List;
+
+import lab2.DirectedGraph.Node;
+
+public class RecursiveSearch {
+
+	private Node<Integer> start;
+	private Node<Integer> end;
+	private Node<Integer> current;
+	
+	private void findPaths(DirectedGraph dg, List<Integer> visits, List<List<Integer>> paths, Node<Integer> current) {
+		if (current.equals(end)) {
+			
+		}
+	}
+	
+}
