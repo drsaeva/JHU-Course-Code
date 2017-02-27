51b3f4c7276f8376d5cf0176814a9d6d6df65864
David R. Saeva <drsaeva@gmail.com>
2017-02-27 18:27:29 -0500

    Update HW1.R

--- BFX/AGGA/HW1.R
+++ BFX/AGGA/HW1.R
@@ -22,65 +22,92 @@ apt-probeset-summarize -a rma-sketch
 ## R code
 # read in exon annotation file, exon and gene matrices
 
-exon <- read.table("D:/Data/cel/exon_output/rma-sketch.summary.txt", skip=121, header=T)
-colnames(exon)
+exon <- read.table("D:/Data/cel/exon_output/rma-sketch.summary.txt", skip=59, header=T, row.names=1, sep="\t")
+gene <- read.table("D:/Data/cel/gene_output/rma-sketch.summary.txt", skip=59, header=T, row.names=1, sep="\t")
 nrow(exon)
-
-gene <- read.table("D:/Data/cel/gene_output/rma-sketch.summary.txt", skip=121, header=T)
+nrow(gene)
 ex_annot <- read.table("D:/Data/cel/HuEx-1_0-st-v2.na24.hg18.probeset_abbr.csv", header=T, sep=",")
 
-gene.dat <- gene[,2:16]
-exon.dat <- exon[,2:16]
-
-par(mfrow=c(2,1))
-g.pca <- prcomp(t(gene.dat))
-plot(g.pca$x[,1:2], col=1,pch=c(rep(0,3),rep(1,3),rep(2,3),rep(3,3),rep(4,3)),
+par(mfrow=c(1,2))
+g.pca <- prcomp(t(gene))
+plot(g.pca$x[,1:2],col=c(rep(1,3),rep(2,3),rep(3,3),rep(4,3),rep(5,3)),pch=c(rep(1,3),rep(2,3),rep(3,3),rep(4,3),rep(5,3)),
      main="Two principal components of gene matrix data")
-legend("bottomleft", pch=c(0:4), c("Ctrl","Clotrimazole","Flunarizine","Ctrl-Chlor","Chlorhexidine"))
+legend("bottomleft", col=c(1:5), pch=c(1:5), c("Ctrl","Clotrimazole","Flunarizine","Ctrl-Chlor","Chlorhexidine"))
 
-e.pca <- prcomp(t(exon.dat))
-plot(e.pca$x[,1:2], col=1,pch=c(rep(0,3),rep(1,3),rep(2,3),rep(3,3),rep(4,3)), 
+e.pca <- prcomp(t(exon))
+plot(e.pca$x[,1:2],col=c(rep(1,3),rep(2,3),rep(3,3),rep(4,3),rep(5,3)),pch=c(rep(1,3),rep(2,3),rep(3,3),rep(4,3),rep(5,3)), 
      main="Two principal components of exon matrix data")
-legend("bottomleft", pch=c(0:4), c("Ctrl","Clotrimazole","Flunarizine","Ctrl-Chlor","Chlorhexidine"))
+legend("bottomleft",  col=c(1:5), pch=c(1:5), c("Ctrl","Clotrimazole","Flunarizine","Ctrl-Chlor","Chlorhexidine"))
 
-
-# exon ni value calculation and t-test function
-# takes transcript cluster values, exon probes for this transcript cluster, and classification vector
+# exon.ni function from lecture notes
 exon.ni <- function(genex,exonx,rx) {
-  ni <- t(exonx)-genex
-  ttest <- t(apply(ni,1,t.two,sam=as.logical(r),v=F))
-  return(ttest)
+	ni <- t(t(exonx)-genex)
+	ttest <- t(apply(ni,1,t.two,sam=as.logical(r),v=F))
+	return(ttest)	
 }
 
-#two-sample t-test function (called by an apply() function)
 
+# two-sample t-test function from notes modified to get foldchange
 t.two <- function(x,sam,v=F) {
-  x <- as.numeric(x)
-  out <- t.test(as.numeric(x[sam]),as.numeric(x[!sam]),alternative="two.sided",var.equal=v)
-  o <- as.numeric(c(out$statistic,out$p.value,out$conf.int[1],out$conf.int[2]),out$estimate[2]-out$estimate[1])
-  names(o) <- c("test_statistic","pv","lower_ci","upper_ci","fold_change")
-  return(o)
+	x <- as.numeric(x)
+	out <- t.test(as.numeric(x[sam]),as.numeric(x[!sam]),alternative="two.sided",var.equal=v)
+	o <- as.numeric(c(out$statistic,out$p.value,out$conf.int[1],out$conf.int[2],out$estimate[1]-out$estimate[2]))
+	names(o) <- c("test_statistic","pv","lower_ci","upper_ci","fold_change")
+	return(o)
 }
+
 
 # intersect annotations & exon matrix, subset each by intersection results
 # get unique transcript ids
-ex.probes <- intersect(exon[,1], rownames(ex_annot))
+ex.probes <- intersect(rownames(exon), rownames(ex_annot))
 ex_annot.sub <- ex_annot[ex.probes,]
-exon.lab <- exon.dat
-row.names(exon.lab) <- exon[,1]
-exon.sub <- exon.lab[ex.probes,]
+exon.sub <- exon[ex.probes,]
 unique.ids <- unique(as.character(ex_annot.sub$transcript_cluster_id))
-
-ex.ge.dif <- matrix(data=NA, nrow=nrow(exon.sub), ncol=ncol(exon.sub))
-colnames(ex.ge.dif) <- colnames(exon.dat)
-ex.ge.pvals <- ex.ge.dif
+unique.ids <- intersect(unique.ids, rownames(gene))
 
 # prepare new matrices for t test
 classes <- c(F,F,F,T,T,T)
-ni.results <- data.frame()
+results <- NULL
 
 # loop over clot samples
-for (i in 1:6) {
-  a <- exon.ni(genex=as.numeric(gene.dat[i]), exonx=exon.sub[unique.ids, i], rx=classes)
-  ni.results <- cbind(ni.results, a)
+for (i in unique.ids) {
+	a <- rownames(ex_annot.sub[ex_annot.sub$transcript_cluster_id %in% i,])
+	ni <- as.data.frame(exon.ni(as.numeric(gene[i,10:15]),exon.sub[a,10:15],classes))
+	ni$transcript_id <- rep(i,nrow(ni))
+	results <- rbind(results,ni)
 }
+
+write.table(results, file="D:/Data/hw2/Chlorhexidine_T_Test.txt", col.names=T, row.names=T)
+
+results.b <- NULL
+#  looop over oother samples
+for (i in unique.ids) {
+	a <- rownames(ex_annot.sub[ex_annot.sub$transcript_cluster_id %in% i,])
+	ni <- as.data.frame(exon.ni(as.numeric(gene[i,1:6]),exon.sub[a,1:6],classes))
+	ni$transcript_id <- rep(i,nrow(ni))
+	results.b <- rbind(results.b,ni)
+}
+
+write.table(results.b, file="D:/Data/hw2/Clotrimazole_T_Test.txt", col.names=T, row.names=T)
+
+# chlor probes with |fc| > 1.5 and p<.01, unique probes
+sum(abs(results$fold_change)>log2(1.5) & results$pv<.01, na.rm=T)
+length(unique(results[abs(results$fold_change)>log2(1.5) & results$pv<.01,6]))
+
+# clotr probes with |fc| > 1.5 and p<.01, unique probes
+sum(abs(results.b$fold_change)>log2(1.5) & results.b$pv<.01, na.rm=T)
+length(unique(results[abs(results.b$fold_change)>log2(1.5) & results.b$pv<.01,6]))
+
+# intersect unique transcripts
+common <- intersect(unique(results.b[abs(results.b$fold_change)>log2(1.5) & results.b$pv<.01,6]), 
+                    unique(results[abs(results$fold_change)>log2(1.5) & results$pv<.01,6]))
+
+length(common)
+write.table(common,"D:/Data/hw1/common", row.names=T, col.names=T)
+            
+# write out significant transcripts for each drug
+write.table(results.b[(abs(results.b$fold_change)>log2(1.5) & results.b$pv<.01), "transcript_id"],
+            "D:/Data/hw1/chlor_ids.txt", col.names=F, row.names=F, quote=F)
+            
+write.table(results[(abs(results.b$fold_change)>log2(1.5) & results.b$pv<.01), "transcript_id"],
+            "D:/Data/hw1/clot_ids.txt", col.names=F, row.names=F, quote=F)
