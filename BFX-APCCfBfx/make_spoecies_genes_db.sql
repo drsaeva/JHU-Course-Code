d11b8ec77b2fe4da7a64afb2330ff9608a360358
David R. Saeva <drsaeva@gmail.com>
2017-06-12 20:35:42 -0400

    Create make_spoecies_genes_db.sql

---
+++ BFX-APCCfBfx/make_spoecies_genes_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE species (
+taxonomy_id INT(7) NOT NULL,
+genus VARCHAR(20) NOT NULL,
+species VARCHAR(10) NOT NULL);
+
+CREATE TABLE genes (
+accession CHAR(9) NOT NULL,
+taxonomy_id INT(7) NOT NULL,
+gene_product_name VARCHAR(20) NOT NULL,
+gene VARCHAR(5) NOT NULL);
